a25844df577af95d99927270f1c5013d28150b44
Jason Hamm <jason.robert.hamm@gmail.com>
2022-03-02 23:35:20 -0500

    Add theme provider to storybook/preview
    - Add styled component theme provider boilerplate to `storybook/preview`

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import { ThemeProvider } from "styled-components";
+
+// TODO: add in real themes
+const placeholderTheme1 = {};
+const placeholderTheme2 = {};
+
+const themes = [placeholderTheme1, placeholderTheme2];
+addDecorator(withThemesProvider(themes), ThemeProvider);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +17,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
