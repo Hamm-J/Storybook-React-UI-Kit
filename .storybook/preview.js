904e777577cd4ae8520526b97d0300b2f91c84b9
Jason Hamm <jason.robert.hamm@gmail.com>
2022-03-03 22:51:46 -0500

    Create global values for colors, shadows, fonts
    - Created a `shared` directory for the global `colors`, `shadows`, and
      `fonts` that will be used across stories and components.
    - Import `colors`, `shadows,` and `fonts` global values into `.storybook/preview.js`
      and pass to storybook decorator.
    - `colors`, `shadows`, and `fonts` are combined into a single object called
      `shared` within `preview.js` before being passed to decorator. When I create
      themes for the different buttons, both `colors` and `fonts` will be
      merged with each theme, as they are used throughout the project.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,18 @@
 import { addDecorator } from "@storybook/react";
 import { withThemesProvider } from "storybook-addon-styled-component-theme";
 import { ThemeProvider } from "styled-components";
+import { colors } from "../src/shared/colors";
+import { shadows } from "../src/shared/shadows";
+import { fonts } from "../src/shared/fonts";
 
 // TODO: add in real themes
-const placeholderTheme1 = {};
-const placeholderTheme2 = {};
+// const placeholderTheme1 = {};
 
-const themes = [placeholderTheme1, placeholderTheme2];
+// const themes = [placeholderTheme1, placeholderTheme2];
+
+const shared = { ...colors, ...shadows, ...fonts };
+
+const themes = [shared];
 addDecorator(withThemesProvider(themes), ThemeProvider);
 
 export const parameters = {
---
+++ src/shared/fonts.js
@@ -0,0 +1,14 @@
+export const fonts = {
+  font56Heading1: "normal normal 900 56px/64px Montserrat",
+  font48Heading2: "",
+  font40Heading3: "",
+  font32Heading4: "",
+  font24Heading5: "",
+  font20Heading6: "",
+  font20Paragraph1Bold: "",
+  font20Paragraph1: "",
+  font16Paragraph2Medium: "",
+  font16Paragraph2: "",
+  font13Paragraph3Bold: "",
+  font13Paragraph3: "",
+};
