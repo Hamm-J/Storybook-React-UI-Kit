25de68bd2c5b411f0fc601aba21dd1def210a533
Jason Hamm <jason.robert.hamm@gmail.com>
2022-03-06 14:21:59 -0500

    Complete adding shared fonts and refactor shadows
    - Added in all of the shared fonts
    - Refactored the named of the fonts' names
    - Refactored the name of the shadows' names
    - Add comments to `.storybook/preview.js`

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,23 @@
+// Import boilerplate for styled-components themes in storybook
 import { addDecorator } from "@storybook/react";
 import { withThemesProvider } from "storybook-addon-styled-component-theme";
 import { ThemeProvider } from "styled-components";
+// Import shared colors, shadows, and fonts across stories
 import { colors } from "../src/shared/colors";
 import { shadows } from "../src/shared/shadows";
 import { fonts } from "../src/shared/fonts";
+// Import fonts
 import "@fontsource/roboto";
 import "@fontsource/montserrat";
 
-// TODO: add in real themes
-// const placeholderTheme1 = {};
-
-// const themes = [placeholderTheme1, placeholderTheme2];
-
+// Create shared theme object. This object will be spread into any theme created.
 const shared = { ...colors, ...shadows, ...fonts };
 
+// Pass theme objects into themes array for storybook
 const themes = [shared];
+
+// Pass custom themes array and styled-componenst ThemeProvider to storybook
+// story decorator function
 addDecorator(withThemesProvider(themes), ThemeProvider);
 
 export const parameters = {
--- src/shared/shadows.js
+++ src/shared/shadows.js
@@ -5,8 +5,8 @@ export const shadows = {
     "0px 4px 4px rgba(255, 196, 196, 0.04), 0px 4px 40px rgba(255, 196, 196, 0.24)",
   shadowInputSuccess:
     "0px 4px 4px rgba(39, 174, 96, 0.04), 0px 4px 40px rgba(39, 174, 96, 0.24)",
-  shadowInputHover:
+  shadowHover:
     "0px 4px 4px rgba(51, 51, 51, 0.04), 0px 4px 56px rgba(51, 51, 51, 0.16)",
-  shadowInputActive:
+  shadowActive:
     "0px 4px 4px rgba(51, 51, 51, 0.04), 0px 4px 24px rgba(51, 51, 51, 0.24)",
 };
