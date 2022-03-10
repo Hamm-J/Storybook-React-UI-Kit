24cc3cd77d2fb1cdba4d39aa742b6265e1e849fd
Jason Hamm <jason.robert.hamm@gmail.com>
2022-03-09 20:19:06 -0500

    Refactor Tab Story
    - Use theme values for styling
    - Create different `Tab` states: large, medium, small, and horizontal
    - Add `argType` `action` to `Tab`

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,16 +1,19 @@
 import { TabContainer, Label } from "./Tab.styled";
 import PropTypes from "prop-types";
 
-const Tab = ({ label }) => {
+const Tab = ({ label, tabState, handleClick }) => {
   return (
-    <TabContainer className="tab">
-      <Label className="tab__label">{label}</Label>
+    <TabContainer className="tab" tabState={tabState} onClick={handleClick}>
+      <Label className="label" tabState={tabState}>
+        {label}
+      </Label>
     </TabContainer>
   );
 };
 
 Tab.propTypes = {
   label: PropTypes.string,
+  tabState: PropTypes.oneOf(["large", "medium", "small", "horizontal"]),
 };
 
 export default Tab;
--- src/components/Tab/Tab.stories.js
+++ src/components/Tab/Tab.stories.js
@@ -3,6 +3,9 @@ import Tab from "./Tab";
 export default {
   title: "Components/Tab",
   component: Tab,
+  argTypes: {
+    handleClick: { action: "handleClick" },
+  },
 };
 
 const Template = (args) => <Tab {...args} />;
@@ -10,4 +13,5 @@ const Template = (args) => <Tab {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   label: "Name of Tab",
+  tabState: "large",
 };
