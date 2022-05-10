b29360cb207215235e68b1b2090c954be16801ab
Jason Hamm <jason.robert.hamm@gmail.com>
2022-05-09 21:36:57 -0400

    Fix center alignment of SelectTag input field

--- src/components/SelectTag/SelectTag.styled.js
+++ src/components/SelectTag/SelectTag.styled.js
@@ -38,8 +38,8 @@ export const InputField = styled.input.attrs({ type: "input" })`
   width: 100%;
   min-width: 80%;
   margin: 0;
+  padding: 0;
   border: none;
-  padding: 8px 66px 0 0px;
   background: none;
   outline: none;
   font: ${(props) => props.theme.fontParagraph2};
